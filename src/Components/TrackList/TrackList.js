42533a8117712e040aa2e03b9b2f0dfb5eef257c
Andy Werdin <andy.werdin@gmail.com>
2023-01-04 11:09:57 +0100

    [AW] component js and css created

---
+++ src/Components/TrackList/TrackList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Track from '../Track/Track';
+import './TrackList.css';
+
+const TrackList = () => {
+    return (
+        <div className="TrackList">
+            {/*<!-- You will add a map method that renders a set of Track components  -->'*/}
+        </div>
+    );
+};
+
+export default TrackList;
