f2d52d3685460799a166ebf794d1ad240a2cc2d2
Andy Werdin <andy.werdin@gmail.com>
2023-01-04 11:52:52 +0100

    [AW] create searchResult state in App component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,16 +1,23 @@
+import React, { useState } from 'react';
 import SearchBar from '../SearchBar/SearchBar';
 import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
 import './App.css';
 
 function App() {
+  const [searchResults, setSearchResults] = useState(
+    [
+      {name: 'Rolling In the Deep', artist: 'Adele', album: '21'}
+    ])
+
+
   return (
     <div>
   <h1>Ja<span className="highlight">mmm</span>ing</h1>
   <div className="App">
     {SearchBar}
     <div className="App-playlist">
-      {SearchResults}
+      <SearchResults searchResults={searchResults}/>
       {Playlist}
     </div>
   </div>
